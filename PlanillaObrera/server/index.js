f2805c471c34e4bf34ca0df634a1e0ee4a08eb44
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-03-23 19:07:27 -0600

    Db connection

--- PlanillaObrera/server/index.js
+++ PlanillaObrera/server/index.js
@@ -1,17 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const sql = require('mssql');
-const { ESRCH } = require('constants');
-const app = express();
+var express = require('express');
+var app = express();
 
-let user = 'Admin';
-let password = 'Corne01214!';
-let dbConnString = `mssql://${user}:${password}@localhost/Traning`;
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
 
-app.use(cors());
-app.use(bodyParser.json());
+    // config for your database
+    var config = {
+        user: 'admin',
+        password: 'Proyecto-BD-1',
+        server: 'planillaobrera.cazk1jfc5xpu.us-east-2.rds.amazonaws.com', 
+        database: 'tarea_programada' 
+    };
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000!');
-})
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from dbo.employees', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
