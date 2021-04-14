8b0f1e5ec1bfea9c444accbe5c6ba5f645f0599f
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-04-14 14:36:04 -0600

    commit

--- PlanillaObrera/server/database/database.service.js
+++ PlanillaObrera/server/database/database.service.js
@@ -11,9 +11,39 @@ let config = {
     database: 'tarea_programada' 
 };
 
+//Administrators--------------------------------------
+
 async function getAllAdministrators(){
     await sql.connect(config);
     const result = await sql.query(`select * from dbo.Administrator`);
     return result.recordset;
 }
+//Jobs------------------------------------------------
+async function getAllJobs(){}
+
+async function addNewJob(){}
+async function removeJob(){}
+
+/* could be one function (ask teacher) */
+async function setJobName(){}
+async function setJobSalary(){}
+
+
+
+
+
+//Employees-------------------------------------------
+async function getAllEmployees(){}
+
+async function addNewEmployee(Id, Name, ValueDocumentId){}
+async function removeEmployee(){}
+
+async function setEmployeeName(){}
+async function setEmployeeIdType(){}
+async function setEmployeeValueDocumentId(){}
+async function setEmployeeBirthDay(){}
+async function setEmployeeJob(){}
+async function setEmployeeIdDepartment(){}
+
+
 
--- PlanillaObrera/src/app/tables/employees/employees.component.ts
+++ PlanillaObrera/src/app/tables/employees/employees.component.ts
@@ -8,6 +8,12 @@ import { Employee } from '../../_models/Employee';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
+  
+  employees: Employee[] = [
+    {Id: 0, Name: "Daniel", ValueDocumentId: 0, IdDepartment: 4, JobName: "Programmer", BirthDay: "14-2-2001", Available: true},
+    {Id: 1, Name: "Sebastian", ValueDocumentId: 5, IdDepartment: 9, JobName: "Programmer", BirthDay: "27-11-2001", Available: true},
+    {Id: 2, Name: "Kensilex", ValueDocumentId: 2, IdDepartment: 1, JobName: "Programmer", BirthDay: "17-4-2001", Available: true}
+  ];
 
   constructor() { }
 
